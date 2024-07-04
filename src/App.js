fa1fe89bcc1d6902b495cd5461240e961395afae
Akash19082001 <akash19m08y2001@gmail.com>
2024-07-04 12:42:23 +0530

    modified files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import About from './Components/About';
 import Contect from './Components/Contect';
@@ -17,6 +18,7 @@ function App() {
        <Resume/>
        <Contect/>
        <Footer/>
+       <Analytics/>
     </div>
   );
 }
